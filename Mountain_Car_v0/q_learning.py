c9b261b5fede691b09cb410cca981becb9cc22a7
Aditya Shirwatkar <50446525+hubble-02@users.noreply.github.com>
2020-05-02 23:26:03 +0530

    1st commit

--- Mountain_Car_v0/q_learning.py
+++ Mountain_Car_v0/q_learning.py
@@ -1,36 +1,36 @@
 import gym
 import numpy as np
-from matplotlib import pyplot as plt
 
 env = gym.make('MountainCar-v0')
 N = 20
-#since our observation space is huge, we approximate it into 20 states
-DISCRETE_SIZE = (N,N,env.action_space.n) #20 -> position, 20 -> velocity, env.actionSpace.n -> actions
-# print([N,N] + [env.action_space.n])
+# since our observation space is huge, we approximate it into N states for quick learning
+DISCRETE_SIZE = (N,N,env.action_space.n) # N -> position, N -> velocity, env.actionSpace.n -> actions
 LEARNING_RATE = 0.5
 DISCOUNT = 0.99
 EPISODES = 10000
 EPSILON = 1
-DECAY_RATE = 1.1
+DECAY_RATE = 1.001
 
 def get_discrete_state(state):
+    # transcription into N states
     discrete_state = ((state - env.observation_space.low)*N)/(env.observation_space.high - env.observation_space.low)
     return tuple(discrete_state.astype(np.int)) #since look q_table pass this directly
 
-q_table = np.random.uniform(low = -3, high=3,size=DISCRETE_SIZE)
+# initialize our q_table randomly
+q_table = np.random.uniform(-3,3,DISCRETE_SIZE)
 
+# make our agent learn 'EPISODES' times
 for eps in range(EPISODES):
-    episode_reward = 0
-    done = False
-    state = get_discrete_state(env.reset())
-    # print((q_table[state]))
-    # action = np.argmax(q_table[state])
-    if eps%500 == 0:
+    done = False # represents whether our agent has finished learning in 'eps' episode
+    state = get_discrete_state(env.reset()) # reseting and getting base position everytime our agent starts learning
+
+    if eps%500 == 0: # since rendering slows down the program, we'll render every 500th episode
         render = True
     else:
         render = False
 
     while not done:
+        # here we'll follow an epsilon-greedy policy
         if np.random.random() >= EPSILON:
             action = np.argmax(q_table[state])
         else:
@@ -39,22 +39,23 @@ for eps in range(EPISODES):
                 rand = np.random.randint(0, env.action_space.n)
             action = rand
 
+        # take a step into the environment
         new_state, reward, done, info = env.step(action)
-        new_dstate = get_discrete_state(new_state)
+        new_dis_state = get_discrete_state(new_state)
 
+        # update our q table using Q-Learning
         if not done:
-            q_target = LEARNING_RATE*(reward + (DISCOUNT*np.max(q_table[new_dstate])) - q_table[state][action])
+            q_target = LEARNING_RATE*(reward + (DISCOUNT*np.max(q_table[new_dis_state])) - q_table[state][action])
             q_table[state][action] += q_target
-            # print(info)
         elif new_state[0] >= env.goal_position:
-            print(f"Yay we reached at {eps} with {new_state[0]} from {env.goal_position}")
-            q_table[state][action] = 0
+            print(f"Yay we reached our goal {eps}th episode")
+            q_table[state][action] = 0 # our terminal state
 
         if render == True:
             env.render()
         # action = new_action
-        state = new_dstate
-        episode_reward += reward
+        state = new_dis_state
+
     EPSILON /= DECAY_RATE
-    # print(episode_reward)
+
 env.close()
