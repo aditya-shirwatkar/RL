aa6279d9310d7e1c947b0db4a02930bad1be2714
Aditya Shirwatkar <50446525+hubble-02@users.noreply.github.com>
2020-05-20 15:54:16 +0530

    added dqn agent

--- CartPole_v0/DQN/test_network.py
+++ CartPole_v0/DQN/test_network.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import gym
 from matplotlib import pyplot as plt
 from matplotlib import animation as anime
@@ -10,6 +12,19 @@ yval = []
 def animate(i):
     plt.cla()
     plt.plot(xval, yval)
+
+class DqnAgent(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(4, 572)
+        self.fc2 = nn.Linear(572, 256)
+        self.fc3 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = (self.fc3(x))
+        return x
 
 run = torch.load('4_572_256_2')
 run.eval()
