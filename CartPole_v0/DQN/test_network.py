9f0b6bdd56cb0c3f82546abcbd32bdb26010461a
Aditya Shirwatkar <50446525+hubble-02@users.noreply.github.com>
2020-05-20 15:59:40 +0530

    added labels to graphs

--- CartPole_v0/DQN/test_network.py
+++ CartPole_v0/DQN/test_network.py
@@ -9,6 +9,9 @@ plt.style.use('fivethirtyeight')
 xval = []
 yval = []
 
+plt.ylabel('Reward')
+plt.xlabel('episode')
+
 def animate(i):
     plt.cla()
     plt.plot(xval, yval)
@@ -32,28 +35,27 @@ env = gym.make('CartPole-v0')
 EPISODES = 1000
 
 for eps in range(EPISODES):
-    k = 0
+    batch_reward = 0
     done = False
     state = (torch.Tensor(env.reset()))
     while not done:
-        # print(k)
         output = run(state)
         action = torch.argmax(output).detach().numpy()
-        # print(action)
-        new_state,reward,done,_ = env.step(action)
+        new_state, reward, done, _ = env.step(action)
         new_state = torch.Tensor(new_state)
-        # replay_mem.append((state,action,reward,new_state,output))
         state = new_state
         env.render()
-        k += 1
+        batch_reward += 1
 
     xval.append(eps)
     yval.append(batch_reward)
     ani = anime.FuncAnimation(plt.gcf(), animate, interval=1000)
-    if eps%cart.SHOW_EVERY == 0:
-        plt.tight_layout()
-        plt.draw()
-        plt.pause(0.001)
-        # cart.train(batch_reward)
+    plt.tight_layout()
+    plt.draw()
+    plt.pause(0.001)
 
 env.close()
+plt.close()
+plt.ylabel('Reward')
+plt.xlabel('episode')
+plt.plot(xval, yval)
